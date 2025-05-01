04f45c006504f580379ae91afb940e9e6da6a4ca
Mathias Ruales <mathias.david.ruales@gmail.com>
2025-05-01 08:59:56 -0500

    Se quito la prueba del mensaje de error en el site.js e index.html

--- RualesMProgreso1/wwwroot/js/site.js
+++ RualesMProgreso1/wwwroot/js/site.js
@@ -2,7 +2,3 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
-function showAlert() {
-    alert(" Mi nombre es Mathias. Mi curso es Prog 4. Mi hobby es dormir. ");
-    return true;
-}
